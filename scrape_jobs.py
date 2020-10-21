b5df326b3184114c9203b49e612029f15636705f
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-10-21 15:03:52 -0400

    create a Beautiful Soup object that takes the HTML content scraped instantiate the object/instruct Beautiful Soup to use the appropriate parser.

--- scrape_jobs.py
+++ scrape_jobs.py
@@ -1,4 +1,7 @@
 import requests
+from bs4 import BeautifulSoup
 
 URL = 'https://www.monster.com/jobs/search/?q=Javascript-Engineer&where=Massachusetts'
 page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, 'html.parser')
