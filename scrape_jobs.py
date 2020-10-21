7921516d747f73b844c7b5ea55f691e2b435e831
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-10-21 15:36:09 -0400

    Include missing ')'

--- scrape_jobs.py
+++ scrape_jobs.py
@@ -17,7 +17,7 @@ for job_elem in job_elems:
     location_elem = job_elem.find('div', class_='location')
     if None in (title_elem, company_elem, location_elem):
         continue
-    print(title_elem.text.strip()
-    print(company_elem.text.strip()
-    print(location_elem.text.strip()
+    print(title_elem.text.strip())
+    print(company_elem.text.strip())
+    print(location_elem.text.strip())
     print()
